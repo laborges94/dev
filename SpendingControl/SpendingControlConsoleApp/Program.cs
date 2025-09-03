859ee1ebd74c76db90449d0445974df61d4ade12
lucas <la.borges_94@hotmail.com>
2025-09-02 23:54:19 -0300

    Program.cs fixes.

--- SpendingControl/SpendingControlConsoleApp/Program.cs
+++ SpendingControl/SpendingControlConsoleApp/Program.cs
@@ -2,8 +2,8 @@
 using SpendingControlConsoleApp.Entities;
 
 var user = new User(1, "John Smith", "john@email.com", "password123", DateTime.Now.AddYears(-5));
-var category = new Category(1, "Food", "Expenses with food and supermarket", "Expense", DateTime.Now.AddYears(-5));
-var expense = new Expense(1, 1, category.Id, 150.75, "Credit Card", DateTime.Now);
+var category = new Category(1, "Food", "Expenses with food and supermarket", CategoryType.Expense, DateTime.Now.AddYears(-5));
+var expense = new Expense(1, 1, category.Id, 150.75, PaymentMethod.CreditCard, DateTime.Now);
 var income = new Income(1, 1, 1, 3500.00, DateTime.Now);
 var creditCard = new CreditCard(1, 1, "My Card", DateTime.Now.AddYears(2), "Visa", 5000.00, DateTime.Now.AddMonths(1), DateTime.Now);
 var financialGoal = new FinancialGoal(1, 1, "Trip", 10000.00, 2500.00, DateTime.Now.AddMonths(12), "International trip", DateTime.Now);
@@ -19,8 +19,8 @@ Console.WriteLine(
 
 user.UpdatePersonalInfo("Jane Doe", "jane@email.com");
 user.UpdatePassword("newpassword123");
-category.UpdateCategory("Supermarket", "All supermarket expenses", "Expense");
-expense.UpdateExpense(category.Id, 50.25, "Debit Card");
+category.UpdateCategory("Supermarket", "All supermarket expenses", CategoryType.Expense);
+expense.UpdateExpense(category.Id, 50.25, PaymentMethod.DebitCard);
 income.UpdateIncome(category.Id, 500.00);
 creditCard.UpdateCreditCard("My New Card", DateTime.Now.AddYears(3), "Mastercard", 7000.00, DateTime.Now.AddMonths(2));
 financialGoal.UpdateFinancialGoal("Car", 30000.00, 5000.00, DateTime.Now.AddMonths(24), "Buy a new car");
