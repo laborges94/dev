0655980ed3477ffddd6d0844d7d1f88d8d93e2b8
lucas <la.borges_94@hotmail.com>
2025-09-11 23:42:55 -0300

    FEAT: class installment and adjustments on expense class

--- SpendingControl/SpendingControlConsoleApp/Entities/Expense.cs
+++ SpendingControl/SpendingControlConsoleApp/Entities/Expense.cs
@@ -7,17 +7,20 @@ namespace SpendingControlConsoleApp.Entities
         public int Id { get; private set; }
         public int UserId { get; private set; }
         public int CategoryId { get; private set; }
-        public int? CreditCardId { get; set; }
+        public int? CreditCardId { get; private set; }
         public string? Description { get; private set; }
         public double Amount { get; private set; }
         public PaymentMethod PaymentMethod { get; private set; }
         public PaymentCondition PaymentCondition { get; private set; }
-        public int Installments { get; private set; } = 1;
+        public int Installments { get; private set; }
         public DateTime Date { get; private set; }
+        private readonly List<Installment> _installmentsList = [];
+        public IReadOnlyCollection<Installment> InstallmentsList => _installmentsList.AsReadOnly();
 
         protected Expense() { }
 
-        public Expense(int id, int userId, int categoryId, double amount, PaymentMethod paymentMethod, PaymentCondition paymentCondition, DateTime date, string? description = null, int? creditCardId = null)
+        public Expense(int id, int userId, int categoryId, double amount, PaymentMethod paymentMethod,
+        PaymentCondition paymentCondition, int installments, DateTime date, string? description = null, int? creditCardId = null)
         {
             if (!string.IsNullOrWhiteSpace(description))
                 EntityValidator.ValidateDescriptionLength(description);
@@ -34,8 +37,11 @@ namespace SpendingControlConsoleApp.Entities
             PaymentMethod = paymentMethod;
             PaymentCondition = paymentCondition;
             Date = date;
+            Installments = installments;
         }
-        public void UpdateExpense(int categoryId, double amount, PaymentMethod paymentMethod, PaymentCondition paymentCondition, string? description = null, int? creditCardId = null)
+
+        public void UpdateExpense(int categoryId, double amount, PaymentMethod paymentMethod, int installments,
+        PaymentCondition paymentCondition, string? description = null, int? creditCardId = null)
         {
             if (!string.IsNullOrWhiteSpace(description))
                 EntityValidator.ValidateDescriptionLength(description);
@@ -49,6 +55,17 @@ namespace SpendingControlConsoleApp.Entities
             PaymentMethod = paymentMethod;
             PaymentCondition = paymentCondition;
             CreditCardId = creditCardId;
+            Installments = installments;
+        }
+
+        public void SetInstallments(int installments, List<Installment> installmentsList)
+        {
+            if (installmentsList == null || installmentsList.Count != installments)
+                throw new ArgumentException("Installments list must match the number of installments.", nameof(installmentsList));
+
+            Installments = installments;
+            _installmentsList.Clear();
+            _installmentsList.AddRange(installmentsList);
         }
     }
 
@@ -57,6 +74,7 @@ namespace SpendingControlConsoleApp.Entities
         InCash,
         Installments
     }
+
     public enum PaymentMethod
     {
         Cash,
